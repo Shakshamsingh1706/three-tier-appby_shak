e4c91d8534eda5a7c86294b2264a4f9ed850e3f1
Shaksham Singh <shakshamsingh1706@gmail.com>
2025-07-30 15:38:50 +0000

    Initial commit

---
+++ backend/app.py
@@ -0,0 +1,29 @@
+from flask import Flask
+import mysql.connector
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello from Flask Backend!"
+
+@app.route('/api')
+def api():
+    return "This is the /api endpoint from Backend!"
+
+@app.route('/db')
+def db_check():
+    try:
+        conn = mysql.connector.connect(
+            host='db',
+            user='user',
+            password='pass',
+            database='appdb'
+        )
+        return "Connected to MySQL DB!"
+    except Exception as e:
+        return str(e)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
